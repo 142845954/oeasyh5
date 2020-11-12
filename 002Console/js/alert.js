383183afbb82381129208d95efeead04af2b98ab
142845954 <zhoubin1428345954@126.com>
2020-11-12 17:25:59 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,19 @@
+/*function woDEtaoLu(){
+    alert("你猜一会儿会发生什么");
+    alert("再等等");
+    alert("也许真有奇迹");
+}
+
+
+alert("你怎么打开了这个网页");
+
+woDEtaoLu();
+alert("你相信奇迹吗")
+alert("让我来想你提个问题")
+woDEtaoLu();
+
+alert("果然没有奇迹")
+alert("我们就这样放弃吗")
+woDEtaoLu();*/
+
+alert("2============这是最后一句了，88");
